abdafdca420ef05924568c10deb778eb5910b5ba
sensiz <dev.federicosensi@gmail.com>
2023-01-17 15:27:37 +0100

    relationship complete

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,5 +9,5 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'overview', 'slug', 'cover'];
+    protected $fillable = ['title', 'overview', 'slug', 'cover', 'type_id'];
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
     use HasFactory;
+    /**
+     * Get all of the posts for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
